0722088e46334e64718025262bbe853372415ca9
quincya <qalexander@valuehorizon.com>
2015-09-25 15:44:27 -0400

    Removed pypi notes

--- setup.py
+++ setup.py
@@ -2,24 +2,6 @@
 """
 Python setup file for the countries app.
 
-In order to register your app at pypi.python.org, create an account at
-pypi.python.org and login, then register your new app like so:
-
-    python setup.py register
-
-If your name is still free, you can now make your first release but first you
-should check if you are uploading the correct files:
-
-    python setup.py sdist
-
-Inspect the output thoroughly. There shouldn't be any temp files and if your
-app includes staticfiles or templates, make sure that they appear in the list.
-If something is wrong, you need to edit MANIFEST.in and run the command again.
-
-If all looks good, you can make your first release:
-
-    python setup.py sdist upload
-
 """
 import os
 from setuptools import setup, find_packages
